8d57624364ecbf7757f1f856971e650459cd35e3
Waqar741 <ahmu741@gmail.com>
2025-10-01 21:16:49 +0530

    worked on api and auth

--- memos/models.py
+++ memos/models.py
@@ -14,3 +14,6 @@ class Memo(models.Model):
     def __str__(self):
         return f"{self.title} ({self.owner.username})"
 
+
+
+
--- memos/serializers.py
+++ memos/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 from .models import Memo
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class MemoSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
@@ -7,3 +11,5 @@ class MemoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Memo
         fields = ["id", "owner", "title", "content", "photo", "created_at", "updated_at"]
+
+
